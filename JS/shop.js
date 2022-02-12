4878d33d8e4941f29ffb53432eb659dad6026e2f
developers-codz <kaj123al2002@gmail.com>
2022-02-12 11:21:59 +0530

    fix:indentation

--- JS/shop.js
+++ JS/shop.js
@@ -1,5 +1,5 @@
-const filterBtn = document.querySelector("#filter-btn")
-const filterBody = document.querySelector("#filter-body")
-filterBtn.addEventListener("click",()=>{
-    filterBody.classList.toggle("show-filter-body")
-})
+const filterBtn = document.querySelector("#filter-btn");
+const filterBody = document.querySelector("#filter-body");
+filterBtn.addEventListener("click", () => {
+  filterBody.classList.toggle("show-filter-body");
+});
